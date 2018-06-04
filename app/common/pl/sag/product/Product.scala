73c6d731b754bacd8a9a3592d9f2b0c075d5a384
Grzegorz Lewczuk <lewczuk.grzeg@gmail.com>
2018-06-04 21:11:04 +0200

    Added: - adding subActors - removing subActors - listing subActors - collecting search results

--- app/common/pl/sag/product/Product.scala
+++ app/common/pl/sag/product/Product.scala
@@ -1,15 +1,17 @@
 package pl.sag.product
 
-case class ProductsInfo (productsInfo: List[ProductInfo])
+case class ProductsInfo(productsInfo: List[ProductInfo])
 
 case class ProductInfo(
-  linkPage: String,
-  description: Option[String],
-  title: Option[String],
-  imageUrl: Option[String]
-) {
+                        linkPage: String,
+                        description: Option[String],
+                        title: Option[String],
+                        imageUrl: Option[String]
+                      ) {
+
   override def toString: String = {
     val shortDesc: String = description.get.substring(0, Math.min(description.get.length(), 100))
+
     s"# ${title.get} # $shortDesc... - url($linkPage), img(${imageUrl.get})"
   }
 }
